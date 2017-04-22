c8bc70406c22fc7e457bfa630085d57d971080c1
Joshua Ogden Johnson <josh@stormylabs.com>
2017-04-22 17:54:03 -0400

    weird

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	var xmlDoc;
+
+	$("#output").html("<div class='message'><em>Getting games list...</em></div>");
+
+	$("#selectAll").click(function() {
+		$("input[type=checkbox]").each(function() {
+			$(this).prop('checked', true)
+		});
+	});
+	$("#selectNone").click(function() {
+		$("input[type=checkbox]").each(function() {
+			$(this).prop('checked', false)
+		});
+	});
 
 	function showWorkingMsg() {
 		$("#workingMsg").show();
@@ -8,108 +22,100 @@ $(document).ready(function() {
 		$("#workingMsg").hide();
 	}
 
-	function init(xmlDoc) {
+	function getList(xmlDoc) {
+		var total = 0;
+		var platformSelected = false;
+		$("input[type=checkbox]").each(function() {
+			if ($(this).is(':checked')) {
+				platformSelected = true;
+			}
+		});
+		if (platformSelected) {
+			$("#details").html("");
+			$("#notes").html("");
+			$("#output").html("<table><thead><tr><td>Title</td><td>Platform</td><td>Notes</td></tr></thead><tbody></tbody></table>");
+
+			$(xmlDoc).find("GAME").each(function(i) {
+				var name = $(this).find("NAME").text();
+				var platform = $(this).find("PLATFORM").text();
+				var notes = $(this).find("NOTES").text();
+				var $checkbox = $("#" + platform);
+				if ($checkbox.is(':checked')) {
+					$("#output tbody").append("<tr><td class='name'>" + name + "</td><td class='platform'>" + platform + "</td><td class='notes'>" + notes + "</td></tr>")
+					total++;
+				} else {
+					//skip
+				}
+			});
+			$("#details").html("Total: " + total + " games.");
+		} else {
+			$("#output").html("<div class='error'>You have to select at least one platform first you noob!</div>");
+		}
+		hideWorkingMsg();
+	}
+
+	function getSuggestion(xmlDoc) {
+		var gamesArray = [];
+		var platformSelected = false;
+		$("input[type=checkbox]").each(function() {
+			if ($(this).is(':checked')) {
+				platformSelected = true;
+			}
+		});
+		if (platformSelected) {
+			//TODO - can we improve performance by refactoring this?
+			$(xmlDoc).find("GAME").each(function(i) {
+				var name = $(this).find("NAME").text();
+				var platform = $(this).find("PLATFORM").text();
+				var notes = $(this).find("NOTES").text();
+				var game = {
+					name : name,
+					platform : platform,
+					notes : notes
+				};
+
+				var $checkbox = $("#" + platform);
+
+				if ($checkbox.is(':checked')) {
+					gamesArray.push(game);
+				} else {
+					//skip
+				}
+			});
+			var random = gamesArray[Math.floor(Math.random() * gamesArray.length)]
+			$("#output").html("<div class='suggestion'>You should play <strong>" + random.name + "</strong> (" + random.platform + ")</div>");
+			$("#details").html("Suggestion was chosen from a list of " + gamesArray.length + " games.");
+
+			if (random.notes != "") {
+				$("#notes").html("Notes: " + random.notes);
+			} else {
+				$("#notes").html("");
+			}
+		} else {
+			$("#output").html("<div class='error'>You have to select at least one platform first you noob!</div>");
+		}
+		hideWorkingMsg();
+	}
+
+	function init() {
 		$("#output").html("<div class='message'><em>Games list retrieved successfully. Standing by...</em></div>");
-		$("#selectAll").click(function() {
-			$("input[type=checkbox]").each(function() {
-				$(this).prop('checked', true)
-			});
-		});
-		$("#selectNone").click(function() {
-			$("input[type=checkbox]").each(function() {
-				$(this).prop('checked', false)
-			});
-		});
 		$("#suggest").removeClass("disabled");
 		$("#listAll").removeClass("disabled");
 		$("#suggest").click(function() {
 			showWorkingMsg();
-			var gamesArray = [];
-			var platformSelected = false;
-			$("input[type=checkbox]").each(function() {
-				if ($(this).is(':checked')) {
-					platformSelected = true;
-				}
-			});
-			if (platformSelected) {
-				//TODO - can we improve performance by refactoring this?
-				$(xmlDoc).find("GAME").each(function(i) {
-					var name = $(this).find("NAME").text();
-					var platform = $(this).find("PLATFORM").text();
-					var notes = $(this).find("NOTES").text();
-					var game = {
-						name : name,
-						platform : platform,
-						notes : notes
-					};
-
-					var $checkbox = $("#" + platform);
-
-					if ($checkbox.is(':checked')) {
-						gamesArray.push(game);
-					} else {
-						//skip
-					}
-				});
-				var random = gamesArray[Math.floor(Math.random() * gamesArray.length)]
-				$("#output").html("<div class='suggestion'>You should play <strong>" + random.name + "</strong> (" + random.platform + ")</div>");
-				$("#details").html("Suggestion was chosen from a list of " + gamesArray.length + " games.");
-
-				if (random.notes != "") {
-					$("#notes").html("Notes: " + random.notes);
-				} else {
-					$("#notes").html("");
-				}
-
-			} else {
-				$("#output").html("<div class='error'>You have to select at least one platform first you noob!</div>");
-			}
-			hideWorkingMsg();
+			getSuggestion(xmlDoc);
 		});
-
 		$("#listAll").click(function() {
-			//TODO: Some of this doesn't need to be repeated.
-			//TODO: why does this message not show?
 			showWorkingMsg();
-			var gamesArray = [];
-			var platformSelected = false;
-			var total = 0;
-			$("input[type=checkbox]").each(function() {
-				if ($(this).is(':checked')) {
-					platformSelected = true;
-				}
-			});
-			if (platformSelected) {
-				$("#details").html("");
-				$("#notes").html("");
-				$("#output").html("<table><thead><tr><td>Title</td><td>Platform</td><td>Notes</td></tr></thead><tbody></tbody></table>");
-
-				$(xmlDoc).find("GAME").each(function(i) {
-					var name = $(this).find("NAME").text();
-					var platform = $(this).find("PLATFORM").text();
-					var notes = $(this).find("NOTES").text();
-					var $checkbox = $("#" + platform);
-					if ($checkbox.is(':checked')) {
-						$("#output tbody").append("<tr><td class='name'>" + name + "</td><td class='platform'>" + platform + "</td><td class='notes'>" + notes + "</td></tr>")
-						total++;
-					} else {
-						//skip
-					}
-				});
-				$("#details").html("Total: " + total + " games.");
-			} else {
-				$("#output").html("<div class='error'>You have to select at least one platform first you noob!</div>");
-			}
-			hideWorkingMsg();
+			getList(xmlDoc);
 		});
 	}
 
-	$("#output").html("<div class='message'><em>Getting games list...</em></div>");
 	$.ajax({
 		url : "/allgames.xml"
 	}).done(function(data) {
-		var xmlDoc = data;
-		init(data);
+		xmlDoc = data;
+		init();
 	}).fail(function(jqXHR, textStatus) {
 		$("#output").html("<div class='error'>Failed to retrieve games list! Error: " + textStatus + "</div>");
 	});
