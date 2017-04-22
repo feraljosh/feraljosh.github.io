b7e286399d16e93cc55276be7b14a1cc66c83670
Joshua Ogden Johnson <josh@stormylabs.com>
2017-04-22 18:32:52 -0400

    getting better

--- js.js
+++ js.js
@@ -14,16 +14,6 @@ $(document).ready(function() {
 		});
 	});
 
-	//function showWorkingMsg() {
-	//console.log('show');
-	//$("#workingMsg").show();
-	//}
-
-	//function hideWorkingMsg() {
-	//console.log('hide');
-	//$("#workingMsg").hide();
-	//}
-
 	function getList(xmlDoc) {
 		var total = 0;
 		var platformSelected = false;
@@ -41,18 +31,19 @@ $(document).ready(function() {
 				var name = this.name;
 				var platform = this.platform;
 				var notes = this.notes;
-				$("#output tbody").append("<tr><td class='name'>" + name + "</td><td class='platform'>" + platform + "</td><td class='notes'>" + notes + "</td></tr>")
-				total++;
-
+				if ($("#" + platform).is(':checked')) {
+					$("#output tbody").append("<tr><td class='name'>" + name + "</td><td class='platform'>" + platform + "</td><td class='notes'>" + notes + "</td></tr>")
+					total++;
+				}
 			});
 			$("#details").html("Total: " + total + " games.");
 		} else {
 			$("#output").html("<div class='error'>You have to select at least one platform first you noob!</div>");
 		}
-		//hideWorkingMsg();
 	}
 
 	function getSuggestion(xmlDoc) {
+		//TODO; move out into single function. Possibly update whenever a checkbox changes, for performance.
 		var platformSelected = false;
 		$("input[type=checkbox]").each(function() {
 			if ($(this).is(':checked')) {
@@ -60,9 +51,18 @@ $(document).ready(function() {
 			}
 		});
 		if (platformSelected) {
-			var random = gamesArray[Math.floor(Math.random() * gamesArray.length)]
+			//same thing - perhaps this subarray can be updated whenever a checkbox changes.
+			var subArray = [];
+			$.each(gamesArray, function() {
+				if ($("#" + this.platform).is(':checked')) {
+					subArray.push(this);
+				}
+			});
+
+			var random = subArray[Math.floor(Math.random() * subArray.length)];
+
 			$("#output").html("<div class='suggestion'>You should play <strong>" + random.name + "</strong> (" + random.platform + ")</div>");
-			$("#details").html("Suggestion was chosen from a list of " + gamesArray.length + " games.");
+			$("#details").html("Suggestion was chosen from a list of " + subArray.length + " games.");
 
 			if (random.notes != "") {
 				$("#notes").html("Notes: " + random.notes);
