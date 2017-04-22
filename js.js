cc735c3eab0d875d803b4156d0f1c7171941ebcc
Joshua Ogden Johnson <josh@stormylabs.com>
2017-04-22 15:49:39 -0400

    Single ajax call, etc.

--- js.js
+++ js.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
-	$("#selectAll").click(function() {
-		$("input[type=checkbox]").each(function() {
-			$(this).prop('checked', true)
+	$("#suggestion").html("Getting games list...");
+	$.ajax({
+		url : "/allgames.xml"
+	}).done(function(data) {
+		var xmlDoc = data;
+		$("#suggestion").html("Games list retrieved. Ready to suggest!");
+		$("#selectAll").click(function() {
+			$("input[type=checkbox]").each(function() {
+				$(this).prop('checked', true)
+			});
 		});
-	});
-	$("#selectNone").click(function() {
-		$("input[type=checkbox]").each(function() {
-			$(this).prop('checked', false)
+		$("#selectNone").click(function() {
+			$("input[type=checkbox]").each(function() {
+				$(this).prop('checked', false)
+			});
 		});
-	});
-	$("#suggest").click(function() {
-		var gamesArray = [];
-		var platformSelected = false;
-
-		$("input[type=checkbox]").each(function() {
-			if ($(this).is(':checked')) {
-				platformSelected = true;
-			}
-		});
-
-		if (platformSelected) {
-			$.ajax({
-				url : "/allgames.xml"
-			}).done(function(data) {
-
-				$(data).find("GAME").each(function(i) {
+		$("#suggest").removeClass("disabled");
+		$("#suggest").click(function() {
+			var gamesArray = [];
+			var platformSelected = false;
+			$("input[type=checkbox]").each(function() {
+				if ($(this).is(':checked')) {
+					platformSelected = true;
+				}
+			});
+			if (platformSelected) {
+				$(xmlDoc).find("GAME").each(function(i) {
 					var name = $(this).find("NAME").text();
 					var platform = $(this).find("PLATFORM").text();
 					var game = {
@@ -35,26 +36,19 @@ $(document).ready(function() {
 					var $checkbox = $("#" + platform);
 
 					if ($checkbox.is(':checked')) {
-						console.log(game);
 						gamesArray.push(game);
 					} else {
 						//skip
 					}
 				});
-
-				var count = gamesArray.length;
-				console.log(count + " games total");
-
 				var random = gamesArray[Math.floor(Math.random() * gamesArray.length)]
-
 				$("#suggestion").html("You should play <strong>" + random.name + "</strong> (" + random.platform + ")");
-
-			}).fail(function(jqXHR, textStatus) {
-				alert("Request failed: " + textStatus);
-			});
-		} else {
-			$("#suggestion").html("You have to select at least one platform first you noob!");
-		}
-
+				$("#details").html("Suggestion was chosen from a list of " + gamesArray.length + " games.");
+			} else {
+				$("#suggestion").html("You have to select at least one platform first you noob!");
+			}
+		});
+	}).fail(function(jqXHR, textStatus) {
+		$("#suggestion").html("Failed to retrieve games list! Error: " + textStatus);
 	});
 });
