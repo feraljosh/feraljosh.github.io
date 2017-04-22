3dd35afdf4f865d23cca6f3c3ed976363ddcd805
Joshua Ogden Johnson <josh@stormylabs.com>
2017-04-22 16:40:31 -0400

    Added List All functionality

--- js.js
+++ js.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
 			});
 		});
 		$("#suggest").removeClass("disabled");
+		$("#listAll").removeClass("disabled");
 		$("#suggest").click(function() {
 			var gamesArray = [];
 			var platformSelected = false;
@@ -47,11 +48,10 @@ $(document).ready(function() {
 				var random = gamesArray[Math.floor(Math.random() * gamesArray.length)]
 				$("#suggestion").html("You should play <strong>" + random.name + "</strong> (" + random.platform + ")");
 				$("#details").html("Suggestion was chosen from a list of " + gamesArray.length + " games.");
-								
-								
-				if(random.notes !=""){
+
+				if (random.notes != "") {
 					$("#notes").html("Notes: " + random.notes);
-				}else{
+				} else {
 					$("#notes").html("");
 				}
 
@@ -59,6 +59,39 @@ $(document).ready(function() {
 				$("#suggestion").html("You have to select at least one platform first you noob!");
 			}
 		});
+
+		$("#listAll").click(function() {
+			//TODO: Some of this doesn't need to be repeated.
+			var gamesArray = [];
+			var platformSelected = false;
+			$("input[type=checkbox]").each(function() {
+				if ($(this).is(':checked')) {
+					platformSelected = true;
+				}
+			});
+			if (platformSelected) {
+
+				$("#details").html("");
+				$("#notes").html("");
+				$("#suggestion").html("<table></table>");
+
+				$(xmlDoc).find("GAME").each(function(i) {
+					var name = $(this).find("NAME").text();
+					var platform = $(this).find("PLATFORM").text();
+					var notes = $(this).find("NOTES").text();
+					var $checkbox = $("#" + platform);
+					if ($checkbox.is(':checked')) {
+						$("#suggestion table").append("<tr><td class='name'>" + name + "</td><td class='platform'>" + platform + "</td><td class='notes'>" + notes + "</td></tr>")
+					} else {
+						//skip
+					}
+				});
+				$("#details").html("Total: " + gamesArray.length + " games.");
+			} else {
+				$("#suggestion").html("You have to select at least one platform first you noob!");
+			}
+		});
+
 	}).fail(function(jqXHR, textStatus) {
 		$("#suggestion").html("Failed to retrieve games list! Error: " + textStatus);
 	});
