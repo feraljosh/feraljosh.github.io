9b16908253b0223848a491ceb555e770b642664c
Joshua Ogden Johnson <josh@stormylabs.com>
2017-04-22 20:46:27 -0400

    CORS

--- js.js
+++ js.js
@@ -1,6 +1,35 @@
-$(document).ready(function() { 
-	
-	https://docs.google.com/spreadsheets/d/1uELdREN_R8thoKZOqJMhgC4hiFOUkcEkb8GJTwhNVaU/edit?usp=sharing
+$(document).ready(function() {
+
+	//https://docs.google.com/spreadsheets/d/1uELdREN_R8thoKZOqJMhgC4hiFOUkcEkb8GJTwhNVaU/edit?usp=sharing
+
+	function createCORSRequest(method, url) {
+		var xhr = new XMLHttpRequest();
+		if ("withCredentials" in xhr) {
+
+			// Check if the XMLHttpRequest object has a "withCredentials" property.
+			// "withCredentials" only exists on XMLHTTPRequest2 objects.
+			xhr.open(method, url, true);
+
+		} else if ( typeof XDomainRequest != "undefined") {
+
+			// Otherwise, check if XDomainRequest.
+			// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+			xhr = new XDomainRequest();
+			xhr.open(method, url);
+
+		} else {
+
+			// Otherwise, CORS is not supported by the browser.
+			xhr = null;
+
+		}
+		return xhr;
+	}
+
+	var xhr = createCORSRequest('GET', url);
+	if (!xhr) {
+		throw new Error('CORS not supported');
+	}
 
 	// ID of the Google Spreadsheet
 	var spreadsheetID = "1uELdREN_R8thoKZOqJMhgC4hiFOUkcEkb8GJTwhNVaU";
